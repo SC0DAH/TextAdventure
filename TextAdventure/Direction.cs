499d9d157884a85d5493058c0ac718f8fb36a59d
SC0DAH <ditissenne@gmail.com>
2025-10-22 13:00:15 +0200

    added Rooms class and Direction class

---
+++ TextAdventure/Direction.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextAdventure
+{
+    internal enum Direction
+    {
+        left, right, up, down
+    }
+}
---
+++ TextAdventure/Rooms.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextAdventure
+{
+    internal class Rooms
+    {
+        public Room CurrentRoom { get; private set; }
+        public Rooms(Room startRoom)
+        {
+            CurrentRoom = startRoom;
+        }
+    }
+}
