2f46a9bfef51fb10d57e5b418c233f121f1aa4b8
SC0DAH <ditissenne@gmail.com>
2025-10-22 12:54:30 +0200

    added Room class

---
+++ TextAdventure/Room.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextAdventure
+{
+    internal class Room
+    {
+        public string Name { get; }
+        public string Description { get; }
+        public bool IsDeadly { get; set; }
+        public bool RequiresKey { get; set; }
+        public bool HasMonster { get; set; }
+        public List<Item> Items { get; } = new List<Item>();
+
+        public Room(string name, string description)
+        {
+            Name = name;
+            Description = description;
+        }
+    }
+}
