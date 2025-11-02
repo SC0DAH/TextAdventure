50ece1bf24e273ffac3081f64fe5edf65ad484e7
Yehor Kroshkin <s154643@ap.be>
2025-11-02 17:21:50 +0100

    moved Steps to another project

---
+++ BehaviorDrivenTesting/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace BehaviorDrivenTesting
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
