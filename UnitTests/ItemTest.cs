62004e2b85ef731da82ce41dfe1feff8f88eca90
Achraf Tazrouti <s161474@ap.be>
2025-11-02 17:11:13 +0100

    edit: deleted TestTextAdventure to UnitTest

---
+++ UnitTests/ItemTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TextAdventure;
+
+namespace UnitTests
+{
+    [TestClass]
+    [TestCategory("Items")]
+    public class ItemTests
+    {
+        [TestMethod]
+        public void Item_ShouldStoreIdAndDescription()
+        {
+            var id = "sword";
+            var description = "A sharp blade.";
+
+            var item = new Item(id, description);
+
+            Assert.AreEqual(id, item.Id);
+            Assert.AreEqual(description, item.Description);
+        }
+    }
+}
