ccb7901d6f55959e9429b6377da26398251df50d
Yehor Kroshkin <s154643@ap.be>
2025-11-02 13:52:40 +0100

    Items Tests toegevoegd

--- UnitTests/ItemsTest.cs
+++ UnitTests/ItemsTest.cs
@@ -3,10 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TextAdventure;
 
 namespace UnitTests
 {
-    internal class ItemsTest
+    public class ItemsTest
     {
+        [TestClass]
+        public class ItemTests
+        {
+            [TestMethod]
+            public void Item_ShouldInitializeCorrectly()
+            {
+                var item = new Item("key", "Golden key");
+
+                Assert.AreEqual("key", item.Id);
+                Assert.AreEqual("Golden key", item.Description);
+            }
+        }
     }
 }
